5f388895b323cf7df80d88f3cb56f7235ff261d9
Srikanth Marrapu <srikanth23111995@gmail.com>
2022-04-12 17:06:06 +0530

    test env added

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,6 +2,8 @@ import type { NextPage } from "next";
 import React from "react";
 import Home from "../lib/components/Home/Home";
 
+console.log(process.env.NEXT_PUBLIC_TEST_VAR);
+
 const Welcome: NextPage = () => {
   return <Home />;
 };
